8b42285ab064daaccdf708e62d04f91b60064634
Holland-Melissa <35241749+Holland-Melissa@users.noreply.github.com>
2018-04-06 13:31:39 -0700

    Delete index.php

--- projectCIT261/php/index.php
+++
@@ -1,9 +0,0 @@
-<?php include 'view/header.php'; ?>
-<main>
-    <ul>
-        <li>
-            <a href="app_manager">To Do List</a>
-        </li>
-    </ul>
-</main>
-<?php include 'view/footer.php'; ?>
