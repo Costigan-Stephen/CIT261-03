8bc1608be0ab79cfd380a4279022c4fbac40e5f8
Holland-Melissa <35241749+Holland-Melissa@users.noreply.github.com>
2018-04-06 13:07:47 -0700

    Create index.php

---
+++ projectCIT261/php/index.php
@@ -0,0 +1,9 @@
+<?php include 'view/header.php'; ?>
+<main>
+    <ul>
+        <li>
+            <a href="app_manager">To Do List</a>
+        </li>
+    </ul>
+</main>
+<?php include 'view/footer.php'; ?>
