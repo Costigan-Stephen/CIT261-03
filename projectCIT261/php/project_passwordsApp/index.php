686e59ca65aade8a3c59e07baa18dd9ea2293153
Holland-Melissa <35241749+Holland-Melissa@users.noreply.github.com>
2018-04-06 21:12:14 -0700

    Delete index.php

--- projectCIT261/php/project_passwordsApp/index.php
+++
@@ -1,9 +0,0 @@
-<?php include '/project_passwordsApp/view/header.php'; ?>
-<main>
-    <ul>
-        <li>
-            <a href="app_manager">User Profiles</a>
-        </li>
-    </ul>
-</main>
-<?php include '/project_passwordsApp/view/footer.php'; ?>
