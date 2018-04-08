fda28396b2be9ee0b431698e0d69f30ccf4bcc8a
Holland-Melissa <35241749+Holland-Melissa@users.noreply.github.com>
2018-04-07 17:14:40 -0700

    Update addprofiles.js

--- projectCIT261/js/addprofiles.js
+++ projectCIT261/js/addprofiles.js
@@ -1,1 +1,80 @@
 
+var profiles = new Array();
+
+window.onload = init;
+
+function init() {
+    var saveButton = document.getElementById("Save");
+    saveButton.onclick = getFormData;
+    getProfileData();
+}
+
+function getProfileData() {
+    var request = new XMLHttpRequest();
+    request.open("GET", "profiles.json");
+    request.onreadystatechange = function() {
+        if (this.readyState == this.DONE && this.status == 200) {
+            if (this.responseText) { 
+                parseProfileItems(this.responseText);
+                addProfilesToPage();
+            }
+            else {
+                console.log("Error: Data is empty");
+            }
+        }
+    };
+    request.send();
+}
+
+function parseProfileItems(profilesJSON) {
+    if (profilesJSON == null || profilesJSON.trim() == "") {
+        return;
+    }
+    var profilesArray = JSON.parse(profilesJSON);
+    if (profilesArray.length == 0) {
+        console.log("Error: the profile list array is empty!");
+        return;
+    }
+    for (var i = 0; i < profilesArray.length; i++) {
+        var profileItem = profilesArray[i];
+        profile.push(profileItem);
+    }
+}
+
+function addProfilesToPage() {
+    var ul = document.getElementById("profileList");
+    for (var i = 0; i < profiles.length; i++) {
+        var profileItem = profiles[i];
+        var li = document.createElement("li");
+        li.innerHTML =
+            profileItem.profile + profileItem.user + profileItem.password + profileItem.email + profileItem.notes;
+        ul.appendChild(li);
+    }
+}
+
+function getFormData() {
+    var profile = document.getElementById("profile").value;
+    if (checkInputText(profile, "Please enter a Profile Name")) return;
+
+    var user = document.getElementById("user").value;
+    if (checkInputText(user, "Please enter a username")) return;
+
+    var password = document.getElementById("password").value;
+    if (checkInputText(password, "Please enter a password")) return;
+
+    var email = document.getElementById("email").value;
+    if (checkInputText(email, "Please enter an email")) return;
+  
+    var notes = document.getElementById("notes").value;
+    if (checkInputText(notes, "Please enter any notes you will need concerning this profile.")) return;
+  
+    console.log("New profile: " + profile + user + password + email + notes);
+}
+
+function checkInputText(value, msg) {
+    if (value == null || value == "") {
+        alert(msg);
+        return true;
+    }
+    return false;
+}         
