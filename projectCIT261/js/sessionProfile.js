a5398d7a0085110d2c6898f7286e9dad791946d7
Costigan-Stephen <stephenjcostigan@hotmail.com>
2018-04-09 00:37:18 -0500

    Update sessionProfile.js

--- projectCIT261/js/sessionProfile.js
+++ projectCIT261/js/sessionProfile.js
@@ -1,66 +1,68 @@
-document.addEventListener("DOMContentLoaded", function startup(){
+document.onload = function startup(){
 	
-	if (window.localStorage.length){	
+	if (localStorage.profiles.id){	
 		var data = loadStorage();
 
 	}else{
-		var idN = sessionStorage[id];
-		var test = [localStorage[profiles[idN].id],localStorage[profiles[idN].user],localStorage[profiles[idN].pass],localStorage[profiles[idN].email],localStorage[profiles[idN].notes]];
-		alert (test);
+		if (sessionStorage.id){
+			//Nothing
+		}else{
+			var idN = sessionStorage[id];
 		
-		document.getElementById('profile').value = localStorage[profiles[idN].id];
-		document.getElementById('user').value = localStorage[profiles[idN].user];
-		document.getElementById('password').value = localStorage[profiles[idN].pass];
-		document.getElementById('email').value = localStorage[profiles[idN].email];
-		document.getElementById('notes').value = localStorage[profiles[idN].notes];
+			var test = [localStorage[profiles[idN].id],localStorage[profiles[idN].user],localStorage[profiles[idN].pass],localStorage[profiles[idN].email],localStorage[profiles[idN].notes]];
+			alert (test);
+			
+			document.getElementById('profile').value = localStorage[profiles[idN].id];
+			document.getElementById('user').value = localStorage[profiles[idN].user];
+			document.getElementById('password').value = localStorage[profiles[idN].pass];
+			document.getElementById('email').value = localStorage[profiles[idN].email];
+			document.getElementById('notes').value = localStorage[profiles[idN].notes];
+		}
 	}
-});
+};
 
 function loadStorage() {
-	if (window.localStorage.length){
-		var obj = { 
-			"profiles" : [
-			{ 
-				"id"  : "Facebook",
-				"user" : "jacksonavery@gmail.com",
-				"pass" : "ilovefacebook",
-				"email" : "jacksonavery@gmail.com",
-				"notes" : ""
-			},
-			{ 
-				"id"  : "Hotmail",
-				"user" : "janesmith@hotmail.com",
-				"pass" : "janesmith54321",
-				"email" : "janesmith@hotmail.com",
-				"notes" : ""
-			},
-			{ 
-				"id"  : "Gmail",
-				"user" : "johndoe@gmail.com",
-				"pass" : "johndoe12345",
-				"email" : "johndoe@gmail.com",
-				"notes" : ""
-			},
-			{ 
-				"id"  : "BYUI",
-				"user" : "student",
-				"pass" : "letsStudyCIT261",
-				"email" : "student@byui.edu",
-				"notes" : ""
-			}
-		]};
-		var profile = new Array(4);
-			profile[0] = obj.profiles[0];
-			profile[1] = obj.profiles[1];
-			profile[2] = obj.profiles[2];
-			profile[3] = obj.profiles[3];
+	
+	var obj = { 
+		"profiles" : [
+		{ 
+			"id"  : "Facebook",
+			"user" : "jacksonavery@gmail.com",
+			"pass" : "ilovefacebook",
+			"email" : "jacksonavery@gmail.com",
+			"notes" : ""
+		},
+		{ 
+			"id"  : "Hotmail",
+			"user" : "janesmith@hotmail.com",
+			"pass" : "janesmith54321",
+			"email" : "janesmith@hotmail.com",
+			"notes" : ""
+		},
+		{ 
+			"id"  : "Gmail",
+			"user" : "johndoe@gmail.com",
+			"pass" : "johndoe12345",
+			"email" : "johndoe@gmail.com",
+			"notes" : ""
+		},
+		{ 
+			"id"  : "BYUI",
+			"user" : "student",
+			"pass" : "letsStudyCIT261",
+			"email" : "student@byui.edu",
+			"notes" : ""
+		}
+	]};
+	var profile = new Array(4);
+		profile[0] = obj.profiles[0];
+		profile[1] = obj.profiles[1];
+		profile[2] = obj.profiles[2];
+		profile[3] = obj.profiles[3];
 		
-		localStorage.setItem("profiles", profile);	
-	}else{
-		var profile = localStorage["profiles"];
-	}
-	
-	
+	localStorage.setItem("profiles", profile);
+	localStorage.getItem("profiles");	
+		
 	var array = [];
 	
 	for(var i=0; i < profile.length; i++) {
