5c88bb568fa38ef02e1619cb9bb304f935c6b954
Costigan-Stephen <stephenjcostigan@hotmail.com>
2018-04-10 11:29:24 -0500

    Update listFunctions.js

--- projectCIT261/js/listFunctions.js
+++ projectCIT261/js/listFunctions.js
@@ -453,3 +453,24 @@ function storageArray(z) {
 	
 	return result;
 }
+
+//################################################################################
+
+function searchFunction() {
+    // Declare variables
+    var input, filter, ul, li, a, i;
+		input = document.getElementById('sch');
+		filter = input.value.toUpperCase();
+		ul = document.getElementById("outputDiv");
+		li = ul.getElementsByTagName('tr');
+
+    // Loop through all list items, and hide those who don't match the search query
+    for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName("td")[0];
+        if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+        }
+    }
+}
